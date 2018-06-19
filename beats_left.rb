aa2f0f95344fb60dc9614fa778e53f359b2d6de1
annaliner <annaliner89@gmail.com>
2018-06-19 20:35:30 +0000

    automatically backed up by learn

--- beats_left.rb
+++ beats_left.rb
@@ -5,7 +5,13 @@ loop do
   if beats_left_in_song == 0 
   break 
 end
-
+ 
+ puts "slide to the left"
+  sleep(0.1)
+  puts "slide to the right"
+  sleep(0.1)
+  puts "and kick"
+  sleep(0.5)
   
    
   
