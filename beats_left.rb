e5fdeb5995e4ec0878a9004096e99a11327f597d
annaliner <annaliner89@gmail.com>
2018-06-19 20:34:23 +0000

    automatically backed up by learn

--- beats_left.rb
+++ beats_left.rb
@@ -2,7 +2,7 @@ def dance
   beats_left_in_song = 60
   
 loop do   
-  if #finish this line, you should stop when beats == 0
+  if beats_left_in_song ==#finish this line, you should stop when beats == 0
   
   
   
